b3bdad05ad8eef4b30a80bcd3bdf77f204256686
romairi <romairi1991@gmail.com>
2023-12-03 13:41:21 +0200

    first lesson

---
+++ firstProject/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'helloWorld';
+  howMany = 5;
+  myArr = ['one', 'two', 'three'];
+
+  myObjsArr = [
+    {
+      fname: 'John',
+      lname: 'Doe',
+      bYear: 2001,
+    },
+    {
+      fname: 'Roman',
+      lname: 'Iri',
+      bYear: 2009,
+    },
+    {
+      fname: 'Ilan',
+      lname: 'Gol',
+      bYear: 2005,
+    },
+  ];
+
+  myMethod() {
+    return 10;
+  }
+
+  shouldShow = true;
+  myVar = true
+}
+~
