d99b662e3c15d6b8767d56841b3f04bfbd9d2b80
romairi <romairi1991@gmail.com>
2023-12-10 10:36:33 +0200

    implement card

---
+++ api-services/src/data/people.ts
@@ -0,0 +1,287 @@
+export const results = [
+  {
+    gender: 'male',
+    name: {
+      title: 'Mr',
+      first: 'Freddie',
+      last: 'Nichols',
+    },
+    location: {
+      street: {
+        number: 135,
+        name: 'James St',
+      },
+      city: 'Orange',
+      state: 'New South Wales',
+      country: 'Australia',
+      postcode: 1215,
+      coordinates: {
+        latitude: '-85.8447',
+        longitude: '-40.6394',
+      },
+      timezone: {
+        offset: '+7:00',
+        description: 'Bangkok, Hanoi, Jakarta',
+      },
+    },
+    email: 'freddie.nichols@example.com',
+    login: {
+      uuid: '5ac06a2d-70de-4bd6-89ec-c94479d821ee',
+      username: 'whiterabbit796',
+      password: 'ming',
+      salt: 'BaZ7R1xH',
+      md5: '6c4984449bdaabf037502cf69b3a57fe',
+      sha1: 'b4ffd5fa97bd77774182bb3c7fa69b36e4441dcf',
+      sha256:
+        '7c6fc859b4a3dd3069365b75a2e1ccf695c086e253bdc3a013753f7a2d56f395',
+    },
+    dob: {
+      date: '1977-07-18T12:02:23.148Z',
+      age: 46,
+    },
+    registered: {
+      date: '2007-05-19T20:40:06.882Z',
+      age: 16,
+    },
+    phone: '04-6851-0982',
+    cell: '0458-115-132',
+    id: {
+      name: 'TFN',
+      value: '608668237',
+    },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/men/28.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/men/28.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/28.jpg',
+    },
+    nat: 'AU',
+  },
+  {
+    gender: 'female',
+    name: {
+      title: 'Mrs',
+      first: 'Kazimira',
+      last: 'Demich',
+    },
+    location: {
+      street: {
+        number: 9858,
+        name: 'Akademika Butlerova',
+      },
+      city: 'Tokmak',
+      state: 'Luganska',
+      country: 'Ukraine',
+      postcode: 95773,
+      coordinates: {
+        latitude: '-42.8656',
+        longitude: '30.5523',
+      },
+      timezone: {
+        offset: '-3:30',
+        description: 'Newfoundland',
+      },
+    },
+    email: 'kazimira.demich@example.com',
+    login: {
+      uuid: 'd229253a-1eaa-4c45-81ec-5494f1e7f6de',
+      username: 'reddog763',
+      password: 'poopy',
+      salt: 'AY4kfeCT',
+      md5: 'd178dd377b3bc654e2ce71e877a6f5b7',
+      sha1: '332e19fe55001bf103ceb5d4f47bd6e2159b4f0d',
+      sha256:
+        'ca2c56b785684b25a219ad7b7844a21407594bd6fce6acedaf3f36ebf9bf3d3c',
+    },
+    dob: {
+      date: '1977-02-09T14:22:38.563Z',
+      age: 46,
+    },
+    registered: {
+      date: '2008-09-02T02:25:36.282Z',
+      age: 15,
+    },
+    phone: '(099) Q64-1217',
+    cell: '(097) E27-7860',
+    id: {
+      name: '',
+      value: null,
+    },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/women/72.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/women/72.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/women/72.jpg',
+    },
+    nat: 'UA',
+  },
+  {
+    gender: 'male',
+    name: {
+      title: 'Mr',
+      first: 'Adam',
+      last: 'Chu',
+    },
+    location: {
+      street: {
+        number: 8027,
+        name: 'Wellington St',
+      },
+      city: 'Deer Lake',
+      state: 'Yukon',
+      country: 'Canada',
+      postcode: 'E4K 7C3',
+      coordinates: {
+        latitude: '-60.7229',
+        longitude: '-150.2289',
+      },
+      timezone: {
+        offset: '+4:00',
+        description: 'Abu Dhabi, Muscat, Baku, Tbilisi',
+      },
+    },
+    email: 'adam.chu@example.com',
+    login: {
+      uuid: '17bbb2b9-5cea-4961-83ca-23bbea025039',
+      username: 'smallsnake671',
+      password: 'angel',
+      salt: 'EWA8c1rC',
+      md5: '4476cef3dd8598b379b3027e2e71ec4a',
+      sha1: '9e429daa249e1a4a46c7bffdfaca420b92c6e04e',
+      sha256:
+        '232c41e018499727bbe11f470d0ebe35c4bbbcd9b4559130738bcc064476e867',
+    },
+    dob: {
+      date: '1987-11-23T14:18:46.043Z',
+      age: 36,
+    },
+    registered: {
+      date: '2014-01-20T21:03:10.386Z',
+      age: 9,
+    },
+    phone: 'V97 E90-2538',
+    cell: 'J48 M43-7151',
+    id: {
+      name: 'SIN',
+      value: '398358408',
+    },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/men/33.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/men/33.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/33.jpg',
+    },
+    nat: 'CA',
+  },
+  {
+    gender: 'male',
+    name: {
+      title: 'Mr',
+      first: 'Maanas',
+      last: 'Shukla',
+    },
+    location: {
+      street: {
+        number: 9265,
+        name: 'China Bazaar Rd',
+      },
+      city: 'Fatehpur',
+      state: 'Uttarakhand',
+      country: 'India',
+      postcode: 49616,
+      coordinates: {
+        latitude: '54.5004',
+        longitude: '162.2196',
+      },
+      timezone: {
+        offset: '+11:00',
+        description: 'Magadan, Solomon Islands, New Caledonia',
+      },
+    },
+    email: 'maanas.shukla@example.com',
+    login: {
+      uuid: '4f37c6f0-d471-4cda-895d-fc2f0385e57d',
+      username: 'beautifulbear820',
+      password: 'salvador',
+      salt: '62Jn1PTw',
+      md5: 'c008b883278b4d8429b1736866500ec3',
+      sha1: '93e8e06215c5b18796fd1b8c7043cd09a2a1078d',
+      sha256:
+        '24efdb07a709e8bc5aff3c6f9230366f8339685f8f2f37b618b51962df55c92b',
+    },
+    dob: {
+      date: '1950-09-22T10:43:32.726Z',
+      age: 73,
+    },
+    registered: {
+      date: '2018-12-19T09:36:23.163Z',
+      age: 4,
+    },
+    phone: '7812180186',
+    cell: '9514710545',
+    id: {
+      name: 'UIDAI',
+      value: '362512956961',
+    },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/men/15.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/men/15.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/15.jpg',
+    },
+    nat: 'IN',
+  },
+  {
+    gender: 'male',
+    name: {
+      title: 'Mr',
+      first: 'Oskari',
+      last: 'Aho',
+    },
+    location: {
+      street: {
+        number: 8572,
+        name: 'Rautatienkatu',
+      },
+      city: 'Raahe',
+      state: 'Central Ostrobothnia',
+      country: 'Finland',
+      postcode: 26481,
+      coordinates: {
+        latitude: '-1.8700',
+        longitude: '-27.4072',
+      },
+      timezone: {
+        offset: '+5:30',
+        description: 'Bombay, Calcutta, Madras, New Delhi',
+      },
+    },
+    email: 'oskari.aho@example.com',
+    login: {
+      uuid: '5b3820e0-f66a-4da5-bc58-d71d276b1596',
+      username: 'blueelephant690',
+      password: '555666',
+      salt: 'Te7NmPT9',
+      md5: 'd9e3704e402d585bdf4b6325af9be4c2',
+      sha1: 'a1626af27208f1876f0c57a4b24ecb4a217e446e',
+      sha256:
+        '66afbcc3bb4f2d972987f9cad0d9db9fa03ac4e2c36ff4a288ecde97a3f99775',
+    },
+    dob: {
+      date: '1998-07-01T16:43:37.435Z',
+      age: 25,
+    },
+    registered: {
+      date: '2017-04-19T18:50:01.617Z',
+      age: 6,
+    },
+    phone: '05-451-913',
+    cell: '044-858-03-68',
+    id: {
+      name: 'HETU',
+      value: 'NaNNA333undefined',
+    },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/men/30.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/men/30.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/30.jpg',
+    },
+    nat: 'FI',
+  },
+];
