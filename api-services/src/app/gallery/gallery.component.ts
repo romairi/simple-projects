6f0d97bea80ce03e0cfc53f4f6ec10457886d564
romairi <romairi1991@gmail.com>
2023-12-10 14:59:54 +0200

    fetch without library

--- api-services/src/app/gallery/gallery.component.ts
+++ api-services/src/app/gallery/gallery.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Person } from 'src/types/Person';
 import { PeopleInfoService } from '../people-info.service';
-import { PersonFromAPI } from '../../types/PersonFromAPI';
 
 @Component({
   selector: 'app-gallery',
@@ -10,30 +9,18 @@ import { PersonFromAPI } from '../../types/PersonFromAPI';
 })
 export class GalleryComponent implements OnInit {
   peopleArr: Person[] = [];
-  url = 'https://randomuser.me/api/?results=50';
 
-  getMyGalleryData() {
-    fetch(this.url)
-      .then((dataFromApi) => {
-        return dataFromApi.json();
-      })
-      .then((dataAsObj) => {
-        this.peopleArr = dataAsObj.results.map((p: PersonFromAPI): Person => {
-          return {
-            firstName: p.name.first,
-            lastName: p.name.last,
-            email: p.email,
-            picUrl: p.picture.large,
-          };
-        });
-      })
-      .catch(() => {});
+  myMessageFromService(data: Person[]) {
+    this.peopleArr = data;
   }
 
   constructor(private peopleInfoService: PeopleInfoService) {}
 
   ngOnInit(): void {
     // this.peopleArr = this.peopleInfoService.getAllPeopleDataStatic();
-    this.getMyGalleryData();
+    this.peopleInfoService.registerToDataUpdates(
+      this.myMessageFromService.bind(this)
+    );
+    this.peopleInfoService.getMyGalleryData();
   }
 }
--- api-services/src/app/people-info.service.ts
+++ api-services/src/app/people-info.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import { results } from 'src/data/people';
+import { Person } from 'src/types/Person';
+import { PersonFromAPI } from 'src/types/PersonFromAPI';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PeopleInfoService {
+  theArr: Person[] = [];
+  listenerArr: Function[] = [];
+  url = 'https://randomuser.me/api/?results=50';
+
   constructor() {}
 
   getAllPeopleDataStatic() {
@@ -18,7 +24,30 @@ export class PeopleInfoService {
     });
   }
 
-  getAllPeopleDataFromWeb() {
-    
+  getAllPeopleDataFromWeb() {}
+
+  getMyGalleryData() {
+    fetch(this.url)
+      .then((dataFromApi) => {
+        return dataFromApi.json();
+      })
+      .then((dataAsObj) => {
+        this.theArr = dataAsObj.results.map((p: PersonFromAPI): Person => {
+          return {
+            firstName: p.name.first,
+            lastName: p.name.last,
+            email: p.email,
+            picUrl: p.picture.large,
+          };
+        });
+        for (const curr of this.listenerArr) {
+          curr(this.theArr);
+        }
+      })
+      .catch(() => {});
+  }
+
+  registerToDataUpdates(func: Function) {
+    this.listenerArr.push(func);
   }
 }
