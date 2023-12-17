8bb6cb7f792b8bf7ea74ae08a58c98260d78b07b
romairi <romairi1991@gmail.com>
2023-12-17 09:40:14 +0200

    create sign-up component

--- api-services/src/app/app.module.ts
+++ api-services/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { CardComponent } from './card/card.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     GalleryComponent,
-    CardComponent
+    CardComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
